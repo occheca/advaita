40c8d2964c97e4cd9c9a3e7eea039f1258bcd3c5
juan.marchal@kantar.com <juan.marchal@kantar.com>
2022-07-18 21:03:08 +0200

    primera_version

---
+++ src/components/Languages.js
@@ -0,0 +1,14 @@
+import './Languages.css';
+
+
+function Languages(){
+    return (
+        <div className="Languages">
+           <span>ESP&nbsp;</span>/<span>&nbsp;ENG</span>
+
+        </div>
+    )
+
+}
+
+export default Languages;
---
+++ src/components/Phrases-container.js
@@ -0,0 +1,19 @@
+import './Phrases-container.css';
+
+function Phrases(){
+    return (
+        <div>
+            <div className="Phrases">
+            Jose Luna es un pensador y escritor.
+
+            </div>
+            
+            <div className="Button">
+                Quiero perder más tiempo    
+            </div>
+        </div>
+    )
+
+}
+
+export default Phrases;
